bb6c4a4cb4df696eee0e636af12bb4d90e5427de
yamatatsu1215 <tatsuyama3140@gmail.com>
2024-04-19 22:54:01 +0900

    react-randam-dog

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,22 @@ import './App.css'
 const dogimage = 'https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg';
 
 export const App = () => {
-  const [dogUrl,setDogUrl] = useState(dogimage); 
+  const [dogUrl,setDogUrl] = useState(dogimage);
+
+  const butttonNewImage = () => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(data => {
+        if (data && data.message) { // データとメッセージが正しく取得できているか確認
+          setDogUrl(data.message);
+        } else {
+          console.error('Error fetching dog image:', data); // エラーの場合はデータをログに出力して確認
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching dog image:', error);
+      });
+  };
   return (
     <div>
       <header>
@@ -17,6 +32,7 @@ export const App = () => {
       </header>
       <p>犬の画像を表示するサイトです。</p>
       <img src={dogUrl} alt="dog" />
+      <button onClick={butttonNewImage}>画像を変更</button>
     </div>
   )
 }
