c0dae7a984433e6a52f14ca20039401056e123dc
yamatatsu1215 <tatsuyama3140@gmail.com>
2024-04-19 22:29:41 +0900

    react-image-usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 // DO NOT DELETE
-
+import { useState } from 'react'
 import './App.css'
 
 /**
  * @type {() => JSX.Element}
  */
+
+const dogimage = 'https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg';
+
 export const App = () => {
+  const [dogUrl,setDogUrl] = useState(dogimage); 
   return (
     <div>
       <header>
         <h1>DOGアプリ</h1>
       </header>
       <p>犬の画像を表示するサイトです。</p>
-      <img src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg" alt="dog" />
+      <img src={dogUrl} alt="dog" />
     </div>
   )
 }
