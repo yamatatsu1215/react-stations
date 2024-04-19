add5eb5d237aaefe0b03e2c8bb24d83aaaa56fbb
yamatatsu1215 <tatsuyama3140@gmail.com>
2024-04-19 22:09:16 +0900

    react-image

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,12 @@ import './App.css'
  */
 export const App = () => {
   return (
-    <header>
-      <h1>DOGアプリ</h1>
-    </header>
+    <div>
+      <header>
+        <h1>DOGアプリ</h1>
+      </header>
+      <p>犬の画像を表示するサイトです。</p>
+      <img src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg" alt="dog" />
+    </div>
   )
 }
