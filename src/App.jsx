a3d605b0189d8dc10a7bd3b4514834f4008b11c8
yamatatsu1215 <tatsuyama3140@gmail.com>
2024-04-22 22:35:12 +0900

    react-select-dogall

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // DO NOT DELETE
 import './App.css'
 import Header from './Header';
-import Description from './Description';
+import DogListContainer from './DogListContainer';
 
 /**
  * @type {() => JSX.Element}
@@ -13,7 +13,7 @@ export const App = () => {
   return (
     <div>
       <Header></Header>
-      <Description></Description>
+      <DogListContainer />
     </div>
   )
 }
--- src/DogListContainer.jsx
+++ src/DogListContainer.jsx
@@ -1,7 +1,72 @@
 // @ts-check
+import { useState, useEffect } from "react"
 
 export const DogListContainer = () => {
-  return <></>
+  // データ一覧を保持するstate
+  const [dogall,dogSelectAll] = useState([]);
+    // 選択された犬種を保持するstate
+  const [selectedBreed, setSelectedBreed] = useState('');
+    // 選択された犬種の画像URL一覧を保持するstate
+  const [breedImages, setBreedImages] = useState([]);
+
+  // データ一覧
+  const fecthDogAll = async () => {
+    try{
+      // 犬の画像を全取得
+      const response = await fetch('https://dog.ceo/api/breeds/list/all');
+      // データをjson形式で取得
+      const data = await response.json();
+      // APIレスポンスから犬のリストの配列を生成して変数に格納
+      const datalist = Object.keys(data.message);
+      dogSelectAll(datalist);
+    } catch (error) {
+      console.error('Error fecthing dog breeds:', error);
+    }
+  };
+
+
+  // データ単品取得
+  const selectfecthDog = async () => {
+    if (selectedBreed){
+      try{
+        const response = await fetch(`https://dog.ceo/api/breed/${selectedBreed}/images`);
+        const data = await response.json();
+        setBreedImages(data.message);
+    } catch (error) {
+      console.error('Error fecthing images for breed ${selectedBreed}:', error);
+    }
+    }
+  };
+  useEffect(()=>{
+    fecthDogAll();
+  },[]);
+
+  useEffect(()=>{
+    selectfecthDog();
+  },[selectedBreed]);
+
+  const handleBreedsChange = (event) => {
+    setSelectedBreed(event.target.value);
+  };
+  return (
+  <div>
+    {/* 犬種の選択 */}
+    <select value={selectedBreed} onChange={handleBreedsChange}>
+      <option value="">Select a breed</option>
+      {dogall.map(breed => (
+        <option value={breed} key={breed}>{breed}</option>
+      ))}
+    </select>
+    {/* 選択された犬種の画像をリスト表示 */}
+    <h2>{selectedBreed}</h2>
+    <div>
+      {breedImages.map(imageUrl => (
+        <img key={imageUrl} src={imageUrl} alt={selectedBreed} />
+      ))}
+    </div>
+
+  </div>
+  )
 }
 
 export default DogListContainer
